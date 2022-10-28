6f091a275b7a87c0dcdaef0220305a8a09ce560e
Lucas Veras <lucasverasspineli@gmail.com>
2022-10-28 00:42:40 -0300

    Criacao da classe ParkingSpotInterface

---
+++ src/main/java/br/com/lucas/api/parking/parkingcontrol/services/resourceinterfaces/ParkingSpotInterface.java
@@ -0,0 +1,9 @@
+package br.com.lucas.api.parking.parkingcontrol.services.resourceinterfaces;
+
+import br.com.lucas.api.parking.parkingcontrol.models.ParkingSpotModel;
+
+public interface ParkingSpotInterface  {
+
+    ParkingSpotModel save(ParkingSpotModel parkingModel);
+
+}
