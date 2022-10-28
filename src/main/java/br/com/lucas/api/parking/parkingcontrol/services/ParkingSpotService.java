8b0783a786c4328b905655bfd2b314534f1837b3
Lucas Veras <lucasverasspineli@gmail.com>
2022-10-28 00:47:53 -0300

    Criacao do método Save

--- src/main/java/br/com/lucas/api/parking/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/br/com/lucas/api/parking/parkingcontrol/services/ParkingSpotService.java
@@ -1,12 +1,21 @@
 package br.com.lucas.api.parking.parkingcontrol.services;
 
+import br.com.lucas.api.parking.parkingcontrol.models.ParkingSpotModel;
 import br.com.lucas.api.parking.parkingcontrol.repositories.ParkingSpotRepository;
+import br.com.lucas.api.parking.parkingcontrol.services.resourceinterfaces.ParkingSpotInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
-public class ParkingSpotService {
+public class ParkingSpotService implements ParkingSpotInterface {
     @Autowired
     private ParkingSpotRepository parkingSpotRepository;
 
+    @Override
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingModel){
+        return parkingSpotRepository.save(parkingModel);
+    }
 }
